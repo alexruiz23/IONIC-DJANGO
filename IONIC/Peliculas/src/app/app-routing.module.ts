2321b98aa113e75325339265f9d2a18f89e913ac
alexruiz23 <alejandroruiztetuan@gmail.com>
2025-02-11 09:38:58 +0100

    Pelis completa

---
+++ IONIC/Peliculas/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+ 
+  {
+    path: 'libros',
+    loadChildren: () => import('./paginas/libros/libros.module').then( m => m.LibrosPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ IONIC/Peliculas/src/app/paginas/libros/libros.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-libros',
+  templateUrl: './libros.page.html',
+  styleUrls: ['./libros.page.scss'],
+  standalone:false
+})
+export class LibrosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
