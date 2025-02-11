1009e10fd1662625418d8aa68a62bee40869d20e
alexruiz23 <alejandroruiztetuan@gmail.com>
2025-02-11 12:23:01 +0100

    Noticias

--- IONIC/noticias/src/app/services/noticias.service.ts
+++ IONIC/noticias/src/app/services/noticias.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RootObject } from '../interfaces/INoticias';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,11 @@ export class NoticiasService  {
 
   getNoticias(): Observable<RootObject>  {
     return this.http.get<RootObject>("https://newsapi.org/v2/top-headlines?apiKey=026d825ed05f40f68c723cfff5cb2972&country=us");
+  }
+  getTopHeadLines(){
+    return this.http.get<RootObject>(url+'apiKey='+environment.apiKey);
+  }
+  getTopHeadLinesCategory(){
+    return this.http.get<RootObject>(url+'category='+cat=+'&apikey'+environment.apiKey);
   }
 }
--- IONIC/noticias/src/app/tab2/tab2.page.ts
+++ IONIC/noticias/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NoticiasService } from '../services/noticias.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,8 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss'],
   standalone: false,
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  categorias: string[] = ['Business', 'Entertaiment', 'General', 'Health', 'Science', 'Sport', 'Technology'];
+  noticias: [] = [];
 
-  constructor() {}
+  constructor(private noticiasService: NoticiasService) {}
 
+  ngOnInit() {
+    this.cargarNoticias(this.categorias[0]);
+  }
+
+  noticiasporCategoria(event) {
+    this.noticias = [];
+    this.cargarNoticias(event.detail.value);
+  }
+
+  cargarNoticias(cat:string) {
+    this.noticiasService.getTopHeadLinesCategory(cat).subscribe(respuesta => {
+      this.noticias.push(...respuesta.articles);
+    });
+  }
 }
