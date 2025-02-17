c3f628d32f1d0549f2b09ea94cd50cac6096870a
alexruiz23 <alejandroruiztetuan@gmail.com>
2025-02-17 23:08:48 +0100

    caa

---
+++ peliculas/src/app/interfaces/iLibros.ts
@@ -0,0 +1,10 @@
+export interface ILibros {
+  id: number;
+  titulo: string;
+  autor: string;
+  descripcion: string;
+  genero: string;
+  portada: string;
+  editorial: string;
+  publicacion: number;
+}
